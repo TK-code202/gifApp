4e08443030d387b65bb402ca31b8848cc7750022
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2023-08-31 23:35:48 +0100

    add gif.js to js folder

---
+++ src/js/gif.js
@@ -0,0 +1,5 @@
+// export const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${name}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+// export const urlTrend = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`;
+
+// export const urlRandom = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`;
